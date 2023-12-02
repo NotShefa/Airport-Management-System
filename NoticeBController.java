9db463f023aef24ff3d377a696ce71e59326cf51
NotShefa <114923276+NotShefa@users.noreply.github.com>
2023-12-02 23:07:55 +0600

    Add files via upload

---
+++ NoticeBController.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class NoticeBController {
+@FXML
+private TextArea noticetxts;
+@FXML
+private Button update;
+@FXML
+private Label done;
+
+public void update(ActionEvent event) {
+	try {
+        BufferedWriter writer=new BufferedWriter(new FileWriter("Notices.txt",true));
+        writer.write(noticetxts.getText());  
+        writer.close();
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+	done.setText("Updated");
+}
+
+
+}
