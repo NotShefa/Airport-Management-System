49cc32f83387133b1bbb5344792eae4f59212799
NotShefa <114923276+NotShefa@users.noreply.github.com>
2023-12-02 22:58:53 +0600

    Add files via upload

---
+++ DestAdder.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DestAdder {
+	@FXML
+	private TextField dest;
+	@FXML
+	private Label label;
+	
+	public void adder(ActionEvent event) {
+		try {
+            BufferedWriter writer=new BufferedWriter(new FileWriter("Destinations.txt",true));
+            writer.write("\n"+dest.getText());
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+		label.setText("Added");
+	}
+
+}
